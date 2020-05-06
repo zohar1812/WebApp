263006063d29ac97f185f635cd2e9a7814774af0
nastiazaya <nastiazaytzev@gmail.com>
2020-05-07 02:31:54 +0300

    Add Validation input for ID filed in registration page

--- src/public/javascripts/registration.js
+++ src/public/javascripts/registration.js
@@ -2,19 +2,31 @@ const config = require('C:\\Users\\Nastya\\WebstormProjects\\WebApp\\config.js')
 const users = require('C:\\Users\\Nastya\\WebstormProjects\\WebApp\\src\\models\\users.js');
 
 
-function registrationUser(req,res){
+
+function registrationUser(req){
     let data;
-    if(req.body.studentID != null){
-    data = {id:req.body.ID, name: req.body.name, lastName: req.body.lastName, studentID: req.body.studentID,
-        userName: req.body.username, password: req.body.password,type:'student',ans:req.body.ans};
+    if(!validID(req.body.ID,)){
+        return false;
+    } else{
+        if(req.body.studentID != null){
+            data = {id:req.body.ID, name: req.body.name, lastName: req.body.lastName, studentID: req.body.studentID,
+                userName: req.body.username, password: req.body.password,type:'student',ans:req.body.ans};
+        }
+        else{
+            data = {id: req.body.ID, name: req.body.name, lastName: req.body.lastName,
+                userName: req.body.username, password: req.body.password ,type:'regular',ans:req.body.ans};
+        }
+        users.saveUser(data,res);
     }
-    else{
-        data = {id: req.body.ID, name: req.body.name, lastName: req.body.lastName,
-            userName: req.body.username, password: req.body.password ,type:'regular',ans:req.body.ans};
-    }
-    users.saveUser(data,res);
 }
 
+function validID(id){
+    if(id.length != 9)
+    {
+        return false;
+    }
+    return true;
+}
 
 
 module.exports.registrationUser = registrationUser;
