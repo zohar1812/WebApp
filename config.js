4c774ab55458e0b022a5dba1ce49c824097e18b6
mor220 <hasid123@gmail.com>
2020-05-14 00:13:11 +0300

    unit test main page + syleling login

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-	host: '192.168.99.101', // check on your computer
+	host: '192.168.99.100', // check on your computer
 	port: '3306', // default port
 	user: 'root',
 	password: 'root',
