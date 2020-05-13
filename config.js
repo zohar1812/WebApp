fe0367784954b11b9ea8f0c9f0beff5cac3cf146
mor220 <hasid123@gmail.com>
2020-05-13 22:01:48 +0300

    styling login page

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: '192.168.99.100', // check on your computer
-  port: '3306', // default port
-  user: 'root',
-  password: 'root',
-  database: 'supersami',
+	host: '192.168.99.101', // check on your computer
+	port: '3306', // default port
+	user: 'root',
+	password: 'root',
+	database: 'supersami'
 });
 connection.connect((error) => {
-  if (error) console.log(error);
-  else console.log('Database Connected!');
+	if (error) console.log(error);
+	else console.log('Database Connected!');
 });
 
 module.exports.connection = connection;
