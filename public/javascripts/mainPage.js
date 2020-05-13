d7bc87b798cb58ab2fde6f2f9b7626ad1057ff4e
mor220 <hasid123@gmail.com>
2020-05-13 19:41:05 +0300

    unit test main page

--- public/javascripts/mainPage.js
+++ public/javascripts/mainPage.js
@@ -1,45 +1,44 @@
 const productTable = require('../../models/products');
 
-
 function getAllAvailableProducts(callBackFunction) {
-  productTable.getAllProducts((result) => {
-    result = getAvailableProducts(result);
-    callBackFunction(result);
-  });
+	productTable.getAllProducts((result) => {
+		result = getAvailableProducts(result);
+		callBackFunction(result);
+	});
 }
-
 
 function filterProducts(products, attr, keyword) {
-  const filtered = [];
-  keyword = keyword.toLowerCase();
-  for (let i = 0; i < products.length; ++i) {
-    const val = products[i][attr].toLowerCase();
-    if (val.includes(keyword)) {
-      filtered.push(products[i]);
-    }
-  }
-  return filtered;
+	const filtered = [];
+	keyword = keyword.toLowerCase();
+	for (let i = 0; i < products.length; ++i) {
+		const val = products[i][attr].toLowerCase();
+		if (val.includes(keyword)) {
+			filtered.push(products[i]);
+		}
+	}
+	return filtered;
 }
 
-
 function getAvailableProducts(products) {
-  const res = [];
-  for (let i = 0; i < products.length; ++i) {
-    if (products[i].quantity > 0) {
-      res.push(products[i]);
-    }
-  }
-  return res;
+	const res = [];
+	for (let i = 0; i < products.length; ++i) {
+		if (products[i].quantity > 0) {
+			res.push(products[i]);
+		}
+	}
+	return res;
 }
 
 function findIndexProducdID(products, id) {
-  for (let i = 0; i < products.length; ++i) {
-    if (products[i].id == id) {
-      return i;
-    }
-  }
-  return -1;
+	for (let i = 0; i < products.length; ++i) {
+		if (products[i].id == id) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 exports.getAllAvailableProducts = getAllAvailableProducts;
 exports.filterProducts = filterProducts;
+exports.findIndexProducdID = findIndexProducdID;
+exports.getAvailableProducts = getAvailableProducts;
