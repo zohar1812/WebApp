ff061f87facf99743c74918a3aba241a36a226f1
nastiazaya <nastiazaytzev@gmail.com>
2020-05-14 15:51:55 +0300

    config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'eu-cdbr-west-03.cleardb.net', // check on your computer
-  //port: '3306', // default port
+  port: '3306', // default port
   user: 'b309a56c38621f',
   password: '4e9ac4e4',
   database: 'heroku_e0f01176dce3eaf',
