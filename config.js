303f044d315b7fdbefad836af2772fc029be22a5
nastiazaya <nastiazaytzev@gmail.com>
2020-05-14 14:23:01 +0300

    change config for DB

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'eu-cdbr-west-03.cleardb.net', // check on your computer
   port: '3306', // default port
-  user: 'bd1fcc1b71587e',
-  password: '19a8684d',
-  database: 'heroku_c242eafe9967244',
+  user: 'b309a56c38621f',
+  password: '4e9ac4e4',
+  database: 'heroku_e0f01176dce3eaf',
 });
 connection.connect((error) => {
   if (error) console.log(error);
