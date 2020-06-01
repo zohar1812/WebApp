2a1b0551244293486e298e5699c26fefd011d056
nastiazaya <nastiazaytzev@gmail.com>
2020-06-01 16:02:53 +0300

    change sql file

--- supersami.sql
+++ supersami.sql
@@ -82,6 +82,36 @@ UPDATE Products SET description = "For microwave preparation 300g" WHERE id = 3;
 
 insert into Products values(4, "Hamburger", "https://www.kroger.com/product/images/xlarge/front/0001111000917",
                             50 ,"meat", 4, "Zoglovek", "Eight frozen beef burgers");
+insert into Products values(5, "Chiken Breast", "https://www.kroger.com/product/images/xlarge/front/0026082900000",
+                            30 ,"meat", 4, "Tyson", "4 chicken breasts ");
+insert into Products values(6, "Chiken nuggets", "https://www.kroger.com/product/images/medium/front/0002370002847",
+                            40 ,"meat", 4, "Tyson", "One 650g package of Chicken Nuggets");
+insert into Products values(7, "Steak", "https://assets.sainsburys-groceries.co.uk/gol/7915774/1/640x640.jpg",
+                            70 ,"meat", 10, "Zoglovek", "600g Steak.");
+insert into Products values(8, "Homestyle Beef Patties", "https://www.kroger.com/product/images/medium/front/0001111096732",
+                            55 ,"meat", 14, "Tyson", "Six frozen Beef Patties");
+
+insert into Products values(9, "Yolo", "https://super-click.co.il/wp-content/uploads/2019/10/111528.jpg",
+                            4 ,"dairy", 12, "Tnuva", "YOLO white chocolate yogurt with raspberry");
+insert into Products values(10, "Shredded Mozzarella Cheese", "https://www.kroger.com/product/images/medium/front/0001111050159",
+                            20 ,"dairy", 16, "Kroger", "170g of Shredded Mozzarella Cheese");
+insert into Products values(11, "Cream Cheese", "https://www.kroger.com/product/images/medium/front/0001111089202",
+                            15 ,"dairy", 13, "Kroger", "220g of Cream Cheese");
+insert into Products values(12, "Butter", "https://www.kroger.com/product/images/medium/front/0001111089301",
+                            18 ,"dairy", 24, "Kroger", "110g of Butter");
+insert into Products values(13, "Choclate Milk", "https://m.pricez.co.il/ProductPictures/7290000057934.jpg",
+                            10 ,"dairy", 13, "Tnuva", "Liter of Choclate Milk");
+
+ insert into Products values(14, "Chips", "https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg",
+                            3 ,"other", 14, "Elit", "50g of chips");
+insert into Products values(15, "Bamba", "https://m.pricez.co.il/ProductPictures/7290000068787.jpg",
+                            5 ,"other", 14, "Osem", "25g of peanut snack");
+insert into Products values(16, "Nacho Doritos", "https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/Doritos-Nacho-446x303.jpg",
+                            4 ,"Elit", 14, "Osem", "55g of Nacho flavored Doritos snack");
+insert into Products values(17, "Bisli Grill", "https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png",
+                            5 ,"other", 20, "Osem", "35g of grill flavored Bisli ");
+insert into Products values(18, "Ketchup", "https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6929362/Custom/6929362_7290000072623_L.png",
+                            10 ,"other", 14, "Osem", "30g of KetChup");
 
 CREATE TABLE Orders(
 `orderId` INT(11) NOT NULL,
@@ -90,6 +120,15 @@ CREATE TABLE Orders(
 `total_price` FLOAT(11) NOT NULL,
 primary key (`orderId`)
 );
+
+insert into Orders values(1, "regular", "2020-05-30",63);
+insert into Orders values(2, "student", "2020-04-20",378);
+insert into Orders values(3, "regular", "2020-05-11",101);
+insert into Orders values(4, "student", "2020-04-20",47);
+insert into Orders values(5, "regular", "2020-05-11",53);
+insert into Orders values(6, "student", "2020-04-20",42);
+insert into Orders values(7, "student", "2020-04-20",47);
+
 
 CREATE TABLE OrderProducts (
 `orderId` INT(11) NOT NULL,
@@ -103,11 +142,64 @@ CREATE TABLE OrderProducts (
  primary key (`orderId`, `productId`)
 );
 
+insert into OrderProducts values(1, 1, 3,15
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(1, 14, 6,18
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(1, 17, 6,30
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+insert into OrderProducts values(2, 5, 3,90
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Hamburger", "Zoglovek","meat");
+insert into OrderProducts values(2, 6, 6,240
+			,"https://www.kroger.com/product/images/medium/front/0002370002847", "Chiken Breast","Tyson","meat");
+insert into OrderProducts values(2, 7, 3,210
+			,"https://assets.sainsburys-groceries.co.uk/gol/7915774/1/640x640.jpg", "Steak" ,"Zoglovek","meat");
+
+insert into OrderProducts values(3, 1, 4,20
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(3, 14, 7,21
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(3, 17, 8,40
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+insert into OrderProducts values(4, 1, 8,40
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(4, 14, 4,12
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(4, 17, 3,15
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+
+insert into OrderProducts values(5, 1, 5,25
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(5, 14, 6,18
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(5, 17, 2,10
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+
+insert into OrderProducts values(6, 1, 4,20
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(6, 14, 4,12
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(6, 17, 3,15
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+
+insert into OrderProducts values(7, 1, 8,40
+			,"https://m.pricez.co.il/ProductPictures/7290000042442.jpg", "Milk","Tnuva","dairy");
+insert into OrderProducts values(7, 14, 4,12
+			,"https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/446x303-6.jpg", "Chips","Elit","other");
+insert into OrderProducts values(7, 17, 3,15
+			,"https://osemcat.signature-it.com//images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6930925/Custom/6930925_7290000066196_L.png", "Bisli Grill","Osem","other");
+
+
 CREATE TABLE IncomeByType(
 `ptype` VARCHAR(10) NOT NULL,
 `total_income` FLOAT(11) NOT NULL
 );
 
-insert into IncomeByType values("meat",0);
-insert into IncomeByType values("other",0);
-insert into IncomeByType values("dairy",0);
+insert into IncomeByType values("meat",378);
+insert into IncomeByType values("other",171);
+insert into IncomeByType values("dairy",169);
