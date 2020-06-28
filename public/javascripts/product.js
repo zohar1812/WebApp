a790efafc74d65c8a2c92d7ff8b02bbfe46fde1c
Zohar Kahlon <zohark1812@gmail,com>
2020-06-28 18:19:28 +0300

    refactoring

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -90,6 +90,18 @@ function addIfNotExist(temp, result) {
 	}
 }
 
+function isOtherProduct(product1, product2) {
+  return product1.id != product2.productId
+}
+
+function isRecommend(product1, product2) {
+  return product1.category == product2.category && product1.brand == product2.brand
+}
+
+function isElementNotIn(product, products) {
+  return products.findIndex((element) => element == product) == -1
+}
+
 function recommentionProduct(allProducts, productsInCart) {
 	const temp = [];
 	const result = [];
@@ -98,60 +110,17 @@ function recommentionProduct(allProducts, productsInCart) {
 		// eslint-disable-next-line no-plusplus
 		for (let j = 0; j < productsInCart.length; j++) {
 			// eslint-disable-next-line eqeqeq
-			if (allProducts[i].id != productsInCart[j].productId) {
-				// eslint-disable-next-line max-len,eqeqeq
-				if (allProducts[i].category == productsInCart[j].category && allProducts[i].brand == productsInCart[j].brand) {
-					if (productsInCart.findIndex((element) => element == allProducts[i]) == -1) {
-						if (temp.findIndex((element) => element == allProducts[i]) == -1) {
-							temp.push(allProducts[i]);
-						}
-					}
+      if (!isOtherProduct(allProducts[i], productsInCart[j]) && isRecommend(allProducts[i], productsInCart[j])  
+            && isElementNotIn(productsInCart, allProducts[i])  && isElementNotIn(temp, allProducts[i])) {
+              temp.push(allProducts[i]);
 				}
 			}
 		}
-	}
 	addIfNotExist(temp, result);
 	return result;
 }
 
-function recommentionProduct(allProducts, productsInCart) {
-  const temp = [];
-  const result = [];
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < allProducts.length; i++) {
-    // eslint-disable-next-line no-plusplus
-    for (let j = 0; j < productsInCart.length; j++) {
-      // eslint-disable-next-line eqeqeq
-      if (allProducts[i].id != productsInCart[j].productId) {
-        // eslint-disable-next-line max-len,eqeqeq
-        if (allProducts[i].category == productsInCart[j].category && allProducts[i].brand == productsInCart[j].brand) {
-          if ((productsInCart.findIndex((element) => element == allProducts[i])) == -1) {
-            if (temp.findIndex((element) => element == allProducts[i]) == -1) {
-              temp.push(allProducts[i]);
-            }
-          }
-        }
-      }
-    }
-  }
-  let flag;
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < temp.length; i++) {
-    flag = true;
-    // eslint-disable-next-line no-plusplus
-    for (let j = 0; j < productsInCart.length;j++) {
-      // eslint-disable-next-line eqeqeq
-      if (temp[i].id == productsInCart[j].productId) {
-        flag = false;
-        break;
-      }
-    }
-    if (flag == true) {
-      result.push(temp[i]);
-    }
-  }
-  return result;
-}
+
 exports.deleteProduct = deleteProduct;
 exports.editProduct = editProduct;
 exports.addProduct = addProduct;
