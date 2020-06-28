7ee07bba228537f31abbf4e80d0b4f38e9ef7318
Zohar Kahlon <zohark1812@gmail,com>
2020-06-28 17:53:09 +0300

    refactoring

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -70,6 +70,50 @@ function validAmound(amound) {
   return true;
 }
 
+// removes all products from the recommendations that already in the cart
+function addIfNotExist(temp, result) {
+	let flag;
+	// eslint-disable-next-line no-plusplus
+	for (let i = 0; i < temp.length; i++) {
+		flag = true;
+		// eslint-disable-next-line no-plusplus
+		for (let j = 0; j < productsInCart.length; j++) {
+			// eslint-disable-next-line eqeqeq
+			if (temp[i].id == productsInCart[j].productId) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag == true) {
+			result.push(temp[i]);
+		}
+	}
+}
+
+function recommentionProduct(allProducts, productsInCart) {
+	const temp = [];
+	const result = [];
+	// eslint-disable-next-line no-plusplus
+	for (let i = 0; i < allProducts.length; i++) {
+		// eslint-disable-next-line no-plusplus
+		for (let j = 0; j < productsInCart.length; j++) {
+			// eslint-disable-next-line eqeqeq
+			if (allProducts[i].id != productsInCart[j].productId) {
+				// eslint-disable-next-line max-len,eqeqeq
+				if (allProducts[i].category == productsInCart[j].category && allProducts[i].brand == productsInCart[j].brand) {
+					if (productsInCart.findIndex((element) => element == allProducts[i]) == -1) {
+						if (temp.findIndex((element) => element == allProducts[i]) == -1) {
+							temp.push(allProducts[i]);
+						}
+					}
+				}
+			}
+		}
+	}
+	addIfNotExist(temp, result);
+	return result;
+}
+
 function recommentionProduct(allProducts, productsInCart) {
   const temp = [];
   const result = [];
